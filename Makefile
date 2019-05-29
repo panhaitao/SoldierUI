9b4d39b3941707e07df9ccb7683f3f8f4d632260
shenlan <xz@onwal.net>
2019-05-29 12:10:15 +0800

    update

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 docker-run: build-image
-	docker run -d -t -i -v /home/shenlan/workspaces/SoldierUI/markdown:/blog/markdown -p 80:5000 --name markdownblog soldierweb 
+	#docker run -d -t -i -v /home/shenlan/workspaces/SoldierUI/markdown:/blog/markdown -p 80:5000 --name markdownblog soldierweb 
+	docker run -d -t -i -v /home/shenlan/workspace/SoldierNote/Archive/:/blog/markdown -p 80:5000 --name MarkdownBlog soldierweb
 build-image:
 	docker build -t soldierweb:latest .
 
@@ -9,4 +10,4 @@ install-dev:
 	pip3 install markdown
 	pip3 install flask
 clean:
-	docker rm markdownblog -f
+	docker rm MarkdownBlog -f
--- app.py
+++ app.py
@@ -13,7 +13,7 @@ nav_str='''
         <nav>
           <ol>
             <li><a href=/>深蓝笔记</a></li>
-        	<li><a href="/about">ABOUT</a></li>     
+            <li><a href="/about">ABOUT</a></li>     
           </ol>
         </nav>
 '''
@@ -32,30 +32,41 @@ def get_md_title(filename):
     md_title=""
     with open(filename,'r') as f:
         for line in f:
-            md_title=line
+            md_title=line.replace(" ","").replace("#","").replace("\n","")
             break
     return md_title
+
+def get_md_category(filename):
+    return filename.split('/')[2]
 
 def get_md_content(filename):
     content=[]
     with open(filename,'r') as f:
         for line in f:
             if re.match(r'^#',line):
-               content.append(line)
+               content.append(line.replace("#","&nbsp;").replace("\n",""))
     return content
+
 
 @app.route('/')
 def index():
     data={ }
     main=""
-    content=""
+    category=""
+    H2_tmp=[]
+    H2=[]
     i=1
     for f in list_all_files("./markdown"):
         title=get_md_title(f)
-        data[i]=[title, f]
+        category=get_md_category(f)
+        data[i]=[category, title, f]
         i+=1
+    for k in data.keys():
+        H2_tmp.append(data[k][0])
+    H2=list(set(H2_tmp))
+    print(H2)
 
-    return render_template('index.html', title="首页", page_nav=nav_str,data=data)
+    return render_template('index.html', title="首页", page_nav=nav_str,data=data, H2=H2 )
 
 @app.route('/about')
 def about():
@@ -65,10 +76,6 @@ def about():
     <p> 个人简介 xz@onwalk.net</p>
     <hr>
 
-    <a id=个人简历></a>
-    <h1> 个人简历</h1>
-    <p> 个人简历 xz@onwalk.net</p>
-    <hr>
     '''
     content='''
     <ul>
@@ -76,7 +83,6 @@ def about():
       </br>
       <hr>
       <li><a href="#个人简介" target="_self" >个人简介</a></li>
-      <li><a href="#个人简历" target="_self" >个人简历</a></li>
     </ul>
     '''
     return render_template('page.html', title="简介", page_nav=nav_str, page_main=main, page_content=content)
